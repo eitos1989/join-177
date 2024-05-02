79f6d7156dcfa632a4bf001f64e6262bc63c4b28
Erik Thomsen <erikthomsen1989@gmail.com>
2024-05-02 20:26:24 +0200

    merge all

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,18 @@
 const STORAGE_TOKEN = 'O1D11J8CK4FZFBAEQUP3HNLLLW82B65GL9S2HEBX';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
-
+const STORAGE_CONTACTS_KEY = 'contacts';
+const STORAGE_TASK_KEY = 'tasks';
+const STORAGE_USER_KEY = 'users';
 
 async function setItem(key, value) {
     const payload = { key, value, token: STORAGE_TOKEN };
-    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
+    return await fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
     .then(res => res.json());
 }
 
 async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(res => res.json());
+    return await fetch(url).then(res => res.json());
 }
 
 
