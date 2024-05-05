efe707184a4d38eaf6fd95980daa4453aa07769e
Erik Thomsen <erikthomsen1989@gmail.com>
2024-05-05 10:41:07 +0200

    insert firebase

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,47 @@
-const STORAGE_TOKEN = 'O1D11J8CK4FZFBAEQUP3HNLLLW82B65GL9S2HEBX';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
-const STORAGE_CONTACTS_KEY = 'contacts';
-const STORAGE_TASK_KEY = 'tasks';
-const STORAGE_USER_KEY = 'users';
+const STORAGE_CONTACTS_PATH = 'contacts';
+const STORAGE_TASK_PATH = 'tasks';
+const STORAGE_USER_PATH = 'users';
 
-async function setItem(key, value) {
-    const payload = { key, value, token: STORAGE_TOKEN };
-    return await fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
-    .then(res => res.json());
+async function getData(path="") {
+    let response = await fetch(BASE_URL + path + ".json") ;
+    let responseAsJson = await response.json();
+    console.log(responseAsJson)
 }
 
-async function getItem(key) {
-    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return await fetch(url).then(res => res.json());
+async function postData(path="", data={}) {
+    let response = await fetch(BASE_URL + path + ".json", {
+        method: "POST",
+        header : {
+            "Content-Type" : "application/json",  
+        },
+        body: JSON.stringify(data)
+    });
+    let responseAsJson = await response.json();
+    console.log(responseAsJson)
 }
+
+async function putData(path="", data={}) {
+    let response = await fetch(BASE_URL + path + ".json", {
+        method: "PUT",
+        header : {
+            "Content-Type" : "application/json",  
+        },
+        body: JSON.stringify(data)
+    });
+    let responseAsJson = await response.json();
+    console.log(responseAsJson)
+}
+
+async function deleteData(path="") {
+    let response = await fetch(BASE_URL + path + ".json", {
+        method: "DELETE",
+    });
+    let responseAsJson = await response.json();
+    console.log(responseAsJson)
+}
+
+
 
 
 
